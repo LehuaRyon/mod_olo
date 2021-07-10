7822e68179f9af566344ca0505bdbf45797df3dc
Jasmine Ryon <jryon12@priorypride.net>
2021-07-10 16:41:13 -0700

    defined create method in sesions controller that works with login form view.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,19 @@ class SessionsController < ApplicationController
 
   def new
     @user = User.new
+    if logged_in?
+      redirect_to pizzas_path
+    end
   end
 
   def create
+    user = User.find_by(email: params[:user][:email])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to pizzas_path
+    else
+      render :new
+    end
   end
 
   def destroy
