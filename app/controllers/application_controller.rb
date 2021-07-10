1c4a0c8a6814db4935aecdb77981fcd417a4c370
Jasmine Ryon <jryon12@priorypride.net>
2021-07-10 14:28:13 -0700

    defined home method in Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
+    include ApplicationHelper
 
     def home
+        if !logged_in?
+            redirect_to login_path
+        end
     end
-    
+
 end
