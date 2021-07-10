e524284ca984f6fac75c5a3afbfa4c56b6beda2a
Jasmine Ryon <jryon12@priorypride.net>
2021-07-10 14:24:05 -0700

    defined new, user_params, and set_user methods in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
+    
 
     def new
-
+        @user = User.new
     end
 
     def create
@@ -18,5 +19,15 @@ class UsersController < ApplicationController
 
     def update
 
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end
+
+    def set_user
+        @user = User.find_by_id(params[:id])
     end
 end
