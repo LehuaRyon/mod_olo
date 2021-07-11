aa7a33eb9fb9f454578ecb244f9733cbc98cc19f
Jasmine Ryon <jryon12@priorypride.net>
2021-07-11 14:47:02 -0700

    moved redirect_if_not_logged_in into Application Controller because it doesn't need to be used in views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,13 @@ class ApplicationController < ActionController::Base
         end
     end
 
+    private
+
+    def redirect_if_not_logged_in
+        if !logged_in?
+            # flash[:not_logged_in] = "You need to be logged in first, in order to view what you would like to see."
+            redirect_to login_path
+        end
+    end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,11 +8,4 @@ module ApplicationHelper
         current_user
     end
 
-    def redirect_if_not_logged_in
-        if !logged_in?
-            # flash[:not_logged_in] = "You need to be logged in first, in order to view what you would like to see."
-            redirect_to login_path
-        end
-    end
-
 end
