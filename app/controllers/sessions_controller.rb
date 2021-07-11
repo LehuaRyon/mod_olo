2a3afb63909cb3f2b1fd1d385773f0c373b23fa4
Jasmine Ryon <jryon12@priorypride.net>
2021-07-11 10:56:39 -0700

    fixed login bug. create action did not need to retrieve nested params.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,15 @@
 class SessionsController < ApplicationController
-
+  
   def new
-    @user = User.new
+    # @user = User.new, DO NOT NEED BC NOT CREATING NEW USER, just logging in
     if logged_in?
       redirect_to pizzas_path
     end
   end
 
   def create
-    user = User.find_by(email: params[:user][:email])
-    if user && user.authenticate(params[:user][:password])
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to pizzas_path
     else
