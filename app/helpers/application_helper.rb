f74f94f48b287d406add7e569db3ce6fe5f06e82
Jasmine Ryon <jryon12@priorypride.net>
2021-07-11 14:36:39 -0700

    defined redirect_if_not_logged_in in ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,10 @@ module ApplicationHelper
     end
 
     def redirect_if_not_logged_in
-
+        if !logged_in?
+            # flash[:not_logged_in] = "You need to be logged in first, in order to view what you would like to see."
+            redirect_to login_path
+        end
     end
 
 end
