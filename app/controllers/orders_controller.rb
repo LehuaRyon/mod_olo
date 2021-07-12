7394d152f487ad4bf941f55c3180cea9b375c1c1
Jasmine Ryon <jryon12@priorypride.net>
2021-07-12 15:10:30 -0700

    set up orders controller with all methods

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,3 +1,53 @@
 class OrdersController < ApplicationController
     before_action :redirect_if_not_logged_in
+    before_action :set_order, except: [:index, :new, :create]
+
+    def index
+        @orders = Order.all
+    end
+    
+    def new
+        @order = Order.new
+    end
+
+    def create
+        @order = current_user.orders.build(order_params)
+        if @order.save
+            redirect_to order_path(@order)
+        else
+            render :new
+        end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        if @order.update(order_params)
+            redirect_to order_path(@order)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @order.destroy
+        redirect_to orders_path
+    end
+
+    private
+
+    def order_params
+        params.require(:order).permit(:pickup_name, :special_instructions)
+    end
+
+    def set_pizza
+        @order = Order.find_by_id(params[:id])
+        if !@order
+            redirect_to orders_path
+        end
+    end
 end
--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -3,5 +3,4 @@ class Pizza < ApplicationRecord
     has_many :users, through: :orders
     validates :name, :ingredients, presence: true, uniqueness: true
     validates :size, presence: true
-    validates_inclusion_of :size, in: %w(Mini Mod Mega), message: " is not a valid size. Enter 'Mini', 'Mod', or 'Mega'"
 end
