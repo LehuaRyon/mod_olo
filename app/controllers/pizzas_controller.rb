2fb1ded0f2510eeec0c314c017e7eba9eed5fff9
Jasmine Ryon <jryon12@priorypride.net>
2021-07-12 13:42:22 -0700

    defined private method set_pizza in pizza controller, and turned it into a before_action for all methods in that controller except index, new, create.

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,5 +1,6 @@
 class PizzasController < ApplicationController
     before_action :redirect_if_not_logged_in
+    before_action :set_pizza, except: [:index, :new, :create]
 
     def index
         @pizzas = Pizza.all
@@ -12,15 +13,25 @@ class PizzasController < ApplicationController
     def create
         @pizza = current_user.pizzas.build(pizza_params)
         if @pizza.save
-            redirect_to pizzas_path
+            redirect_to pizza_path(@pizza)
         else
             render :new
         end
+    end
+
+    def show
+    end
+
+    def edit
     end
 
     private
 
     def pizza_params
         params.require(:pizza).permit(:name, :ingredients, :size)
+    end
+
+    def set_pizza
+        @pizza = Pizza.find_by_id(params[:id])
     end
 end
