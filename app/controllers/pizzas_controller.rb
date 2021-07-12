78801c56442f0de2d026fca8233e87b34a5c2672
Jasmine Ryon <jryon12@priorypride.net>
2021-07-11 17:50:08 -0700

    defined create action in Pizzas Controller and pizza_params.  Can now create new pizza object with relation to current user

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -4,4 +4,19 @@ class PizzasController < ApplicationController
     def new
         @pizza = Pizza.new
     end
+
+    def create
+        @pizza = current_user.pizzas.build(pizza_params)
+        if @pizza.save
+            redirect_to pizzas_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def pizza_params
+        params.require(:pizza).permit(:name, :ingredients, :size)
+    end
 end
--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,7 +1,7 @@
 class Pizza < ApplicationRecord
     has_many :orders, dependent: :destroy
     has_many :users, through: :orders
-    validates :name, :ingredients, presence: :true, uniqueness: true
+    validates :name, :ingredients, presence: true, uniqueness: true
     validates :size, presence: true
-    validates_inclusion_of :size, in: %w(Mini Mod Mega), message: "%(value) is not a valid size"
+    validates_inclusion_of :size, in: %w(Mini Mod Mega), message: " is not a valid size. Enter 'Mini', 'Mod', or 'Mega'"
 end
