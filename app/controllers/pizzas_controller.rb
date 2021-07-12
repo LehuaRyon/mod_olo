da3f0d2da59335ccffb96f67c14bae877db52ac4
Jasmine Ryon <jryon12@priorypride.net>
2021-07-12 13:51:00 -0700

    added a redirect in set_pizza

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -46,5 +46,8 @@ class PizzasController < ApplicationController
 
     def set_pizza
         @pizza = Pizza.find_by_id(params[:id])
+        if !@pizza
+            redirect_to pizzas_path
+        end
     end
 end
