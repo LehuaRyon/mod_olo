7e1a754eecf40a609c755433fbfdc520edea4fcc
Jasmine Ryon <jryon12@priorypride.net>
2021-07-16 21:05:26 -0700

    created validates_uniqueness_of for name, so the same name of a pizza can't have the same size persisted to database. Needs more narrowing down.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
-  belongs_to :pizza, dependent: :destroy
+  belongs_to :pizza
   # accepts_nested_attributes_for :pizza
   validates :pickup_name, :special_instructions, :pizza_id, presence: true
   # validates :special_instructions, allow_blank: true
--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -2,6 +2,7 @@ class Pizza < ApplicationRecord
     has_many :orders, dependent: :destroy
     has_many :users, through: :orders
     validates :name, :ingredients, :size, presence: true
+    validates_uniqueness_of :name, scope: :size
     # can have same name w/same ingredients but not same size for same name, figure out validation
     scope :search_by_pizza_name, -> (search) {where("name LIKE ?", search)}
     # def self.search_by_pizza_name(search)
