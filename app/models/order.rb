cd858a174bed61e133623cd545155a781df8050d
Jasmine Ryon <jryon12@priorypride.net>
2021-07-16 19:41:16 -0700

    added dependent destroy to belong_to association in order model, also added validates_associated pizza, and created a custom writer method for pizza_attributes

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,11 @@
 class Order < ApplicationRecord
   belongs_to :user
-  belongs_to :pizza #, optional: true
+  belongs_to :pizza, dependent: :destroy
   # accepts_nested_attributes_for :pizza
-  validates :pickup_name, :special_instructions, presence: true
+  validates :pickup_name, :special_instructions, :pizza_id, presence: true
   # validates :special_instructions, allow_blank: true
     # when seeding data, allow_blank was not allowing seed
+  validates_associated :pizza
   scope :order_by_created_at, -> {order(created_at: :desc)}
   # def self.order_by_created_at
   #   Order.order(created_at: :desc)
@@ -18,5 +19,10 @@ class Order < ApplicationRecord
       Order.order_by_created_at
     end
   end
+
+  def pizza_attributes=(attributes)
+    pizza = Pizza.find_or_create_by(attributes)
+    self.pizza = pizza if pizza.valid? || !self.pizza #== nil
+  end 
 
 end
