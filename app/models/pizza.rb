492a71be652bfa86dc8bc04f37aba44dfb803632
Jasmine Ryon <jryon12@priorypride.net>
2021-07-16 19:31:32 -0700

    added nested form in new view of orders (to create a new pizza)

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -3,7 +3,6 @@ class Pizza < ApplicationRecord
     has_many :users, through: :orders
     validates :name, :ingredients, :size, presence: true
     # can have same name w/same ingredients but not same size for same name, figure out validation
-
     scope :search_by_pizza_name, -> (search) {where("name LIKE ?", search)}
     # def self.search_by_pizza_name(search)
     #     name = self.find_by_name(search)
