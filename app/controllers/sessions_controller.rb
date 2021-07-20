ceceb090d97e8ec5ee50b54758fc64d63bec9767
Jasmine Ryon <jryon12@priorypride.net>
2021-07-20 12:55:46 -0700

    installed and migrated active storage for images

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -34,14 +34,6 @@ class SessionsController < ApplicationController
     # else
     #   redirect_to root_path
     # end
-
-    # @user = User.from_google_omniauth(auth)
-    # # if @user.valid?
-    #   session[:user_id] = @user.id
-    #   redirect_to user_path(@user)
-    # # else
-    # #   redirect_to root_path
-    # # end
   end
 
   private 
