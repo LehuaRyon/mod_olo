76928a9a60d9567dd423fcd8d8f4f0eb0bc50b29
Jasmine Ryon <jryon12@priorypride.net>
2021-07-21 11:31:45 -0700

    defined create action in reviws controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -16,7 +16,18 @@ class ReviewsController < ApplicationController
     end
 
     def create
-
+        # @review = Review.new(review_params)
+        # @review = @pizza.reviews.build(review_params) to this instead:
+        @review = current_user.reviews.build(review_params)
+        # handling both associations at once, connected to pizza and user
+        @review.pizza = @pizza
+        if @review.save
+            redirect_to pizza_path(@review.pizza)
+        else
+            # set_pizza
+            # @pizza.reviews.build
+            render :new
+        end
     end
 
     private
