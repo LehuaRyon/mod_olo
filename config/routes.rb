d3e0510dc085fc3593a4bb2c96d50fd17e91d837
Jasmine Ryon <jryon12@priorypride.net>
2021-07-21 11:19:52 -0700

    added reviews as a nested resource under pizzas in routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   resources :orders
   resources :pizzas do
      resources :orders, only: [:index, :new, :create]
+     resources :reviews, only: [:index, :new, :create]
   end
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
