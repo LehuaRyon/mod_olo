b5bdf323ec8bfa47c83bf62d0dbcdda46f1cf44d
Jasmine Ryon <jryon12@priorypride.net>
2021-07-21 11:18:12 -0700

    added association to review in user model, sourcing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :orders, dependent: :destroy
   has_many :pizzas, through: :orders
+  has_many :reviews, foreign_key: "reviewer_id", class_name: "Review"
+  has_many :reviewed_pizzas, through: :reviews, source: :pizza
   validates :first_name, :last_name, presence: true
   validates :email, presence: true, uniqueness: true
   validates_length_of :password, minimum: 6, too_short: "Please enter at least 6 characters"
