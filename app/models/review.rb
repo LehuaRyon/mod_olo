f894cad6908f83c0b910c73a203e15c4b65fb7a8
Jasmine Ryon <jryon12@priorypride.net>
2021-07-21 11:15:40 -0700

    set up review model with validations, associations, and a scope method

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,10 @@
 class Review < ApplicationRecord
+    validates :stars, :content, presence: true
+    validates :stars, numericality: { less_than_or_equal_to: 5, greater_than: 0, only_integer: true }
+    belongs_to :pizza
+    belongs_to :reviewer, class_name: "User"
+
+    def self.average_star_rating
+        average(:stars)
+    end
 end
