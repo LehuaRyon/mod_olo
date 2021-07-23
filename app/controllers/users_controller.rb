ef9dfbb027e3f0ca12eede7667cb2ce5c916ade8
Jasmine Ryon <jryon12@priorypride.net>
2021-07-22 17:36:02 -0700

    added edit, update, destroy actions in users controllerand changed user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    # before_action :redirect_if_not_logged_in, except: [:new, :create]
+    before_action :redirect_if_not_logged_in, except: [:new, :create]
     before_action :set_user, except: [:new, :create]
 
     def new
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.new(user_params)
+        @user = User.new(user_params(:first_name, :last_name, :email, :password))
+        # @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
             redirect_to user_path(@user)
@@ -21,24 +22,37 @@ class UsersController < ApplicationController
     end
 
     def show
-        redirect_if_not_logged_in
+        # redirect_if_not_logged_in
         # @user = User.find_by_id(params[:id])
         redirect_to root_path if !@user
         # protection so user never sees error page
     end
 
     def edit
-
     end
 
     def update
+        if @user.update(user_params(:email, :password))
+        # if @user.update(user_params)
+            redirect_to user_path(@user)
+        else
+            render :edit
+        end
+    end
 
+    def destroy
+        @user.destroy
+        redirect_to signup_path
     end
 
     private
 
-    def user_params
-        params.require(:user).permit(:first_name, :last_name, :email, :password)
+    # def user_params
+    #     params.require(:user).permit(:first_name, :last_name, :email, :password)
+    # end
+
+    def user_params(*args)
+        params.require(:user).permit(*args)
     end
 
     def set_user
