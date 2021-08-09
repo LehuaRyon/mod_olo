efeeaae87a9b7258149301d9bfd93711dbd4b87d
Jasmine Ryon <jryon12@priorypride.net>
2021-08-08 21:24:43 -0700

    added password_confirmation to .from_google_omniauth method because a user was not saving to be able to be logged in through google

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,6 @@ class User < ApplicationRecord
   validates :first_name, :last_name, :password_confirmation, presence: true
   validates :email, presence: true, uniqueness: true
   validates_length_of :password, minimum: 6, too_short: "Please enter at least 6 characters"
-  #validates_confirmation_of :password
-  # validates_presence_of :password_confirmation, :if => :password_changed?
 
   def self.from_google_omniauth(auth)
     find_or_create_by(uid: auth[:uid]) do |u|
@@ -16,6 +14,7 @@ class User < ApplicationRecord
       u.last_name = auth[:info][:last_name]
       u.email = auth[:info][:email]
       u.password = SecureRandom.hex(10)
+      u.password_confirmation = u.password
     end
   end
 
