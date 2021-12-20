91576240758b6bff7e5ee559ed8c447bb72ac60c
Jasmine Ryon <jryon12@priorypride.net>
2021-12-19 17:01:26 -0800

    changed a word in readme

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ To install and run the app on your local machine:
 3. Then run `cd mod_olo` to navigate to the mod_olo directory
 4. Run `bundle install` to install the necessary gems and dependencies
 5. Run `rake db:migrate` to perform the database migrations
-6. Run `rake db:migrate` to fill the database with MOD's available classic pizzas
+6. Run `rake db:seed` to fill the database with MOD's available classic pizzas
 7. Run `rails s` to start a local rails server
 8. Go to <a href="localhost:3000" target="_blank">localhost:3000</a> to view the app!
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,30 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# User.create([
-#     {first_name: "Captain", last_name: "Morgan", email: "captain@gmail.com", password: "password"},
-#     {first_name: "Oscar", last_name: "Mayer", email: "oscar@gmail.com", password: "password"},
-#     {first_name: "Betty", last_name: "Crocker", email: "betty@gmail.com", password: "password"}
-#     ])
+User.create([
+    {first_name: "Captain", last_name: "Morgan", email: "captain@gmail.com", password: "password"},
+    {first_name: "Oscar", last_name: "Mayer", email: "oscar@gmail.com", password: "password"},
+    {first_name: "Betty", last_name: "Crocker", email: "betty@gmail.com", password: "password"}
+    ])
 
-# Pizza.create([
-#     {name: "Mad Dog", ingredients: "Red Sauce, mozzarella, pepperoni, mild sausage, ground beef"},
-#     {name: "Tristan", ingredients: "Mozzarella, asiago, roasted red peppers, mushrooms, pesto drizzle finish"},
-#     {name: "Dominic", ingredients: "White Sauce, fresh chopped basil, asiago, mild sausage, red onion, sliced tomatoes"},
-#     {name: "Lucy Sunshine", ingredients: "Mozzarella, parmesan, garlic, artichokes, finished with dollops of red sauce"},
-#     {name: "Jasper", ingredients: "Red Sauce, mozzarella, spicy chicken sausage, mushrooms"},
-#     {name: "Dillion James", ingredients: "Red Sauce, mozzarella, asiago, fresh chopped basil, garlic, sliced tomatoes"},
-#     {name: "Calexico", ingredients: "Red Sauce, mozzarella, gorgonzola, chicken, jalapenos, finished with hot buffalo sauce zigzag"},
-#     {name: "Caspian", ingredients: "BBQ Sauce, mozzarella, gorgonzola, chicken, red onions, finished with BBQ sauce swirl"}
-# ])
+Pizza.create([
+    {name: "Mad Dog", ingredients: "Red Sauce, mozzarella, pepperoni, mild sausage, ground beef"},
+    {name: "Tristan", ingredients: "Mozzarella, asiago, roasted red peppers, mushrooms, pesto drizzle finish"},
+    {name: "Dominic", ingredients: "White Sauce, fresh chopped basil, asiago, mild sausage, red onion, sliced tomatoes"},
+    {name: "Lucy Sunshine", ingredients: "Mozzarella, parmesan, garlic, artichokes, finished with dollops of red sauce"},
+    {name: "Jasper", ingredients: "Red Sauce, mozzarella, spicy chicken sausage, mushrooms"},
+    {name: "Dillion James", ingredients: "Red Sauce, mozzarella, asiago, fresh chopped basil, garlic, sliced tomatoes"},
+    {name: "Calexico", ingredients: "Red Sauce, mozzarella, gorgonzola, chicken, jalapenos, finished with hot buffalo sauce zigzag"},
+    {name: "Caspian", ingredients: "BBQ Sauce, mozzarella, gorgonzola, chicken, red onions, finished with BBQ sauce swirl"}
+])
 
-# Order.create([
-#     {user_id: 1, pizza_id: 1, pickup_name: "Captain Morgan", special_instructions: "3 sides of utensils", size: "Mini (6-inch)"},
-#     {user_id: 1, pizza_id: 2, pickup_name: "Captain Morgan", special_instructions: "Napkins on the side", size: "Mod (11-inch)"},
-#     {user_id: 1, pizza_id: 3, pickup_name: "Captain Morgan", special_instructions: "No onions", size: "Mega (11-inch thick crust)"},
-#     {user_id: 2, pizza_id: 4, pickup_name: "Oscar Mayer", special_instructions: "Extra red sauce on top", size: "Mini (6-inch)"},
-#     {user_id: 2, pizza_id: 5, pickup_name: "Oscar Mayer", special_instructions: "No cheese", size: "Mod (11-inch)"},
-#     {user_id: 2, pizza_id: 6, pickup_name: "Oscar Mayer", special_instructions: "Parmesan and red pepper packets on the side", size: "Mega (11-inch thick crust)"},
-#     {user_id: 3, pizza_id: 7, pickup_name: "Betty Crocker", special_instructions: "Light buffalo sauce on top", size: "Mini (6-inch)"},
-#     {user_id: 3, pizza_id: 8, pickup_name: "Betty Crocker", special_instructions: "Extra chicken ontop.", size: "Mod (11-inch)"}
-# ])
+Order.create([
+    {user_id: 1, pizza_id: 1, pickup_name: "Captain Morgan", special_instructions: "3 sides of utensils", size: "Mini (6-inch)"},
+    {user_id: 1, pizza_id: 2, pickup_name: "Captain Morgan", special_instructions: "Napkins on the side", size: "Mod (11-inch)"},
+    {user_id: 1, pizza_id: 3, pickup_name: "Captain Morgan", special_instructions: "No onions", size: "Mega (11-inch thick crust)"},
+    {user_id: 2, pizza_id: 4, pickup_name: "Oscar Mayer", special_instructions: "Extra red sauce on top", size: "Mini (6-inch)"},
+    {user_id: 2, pizza_id: 5, pickup_name: "Oscar Mayer", special_instructions: "No cheese", size: "Mod (11-inch)"},
+    {user_id: 2, pizza_id: 6, pickup_name: "Oscar Mayer", special_instructions: "Parmesan and red pepper packets on the side", size: "Mega (11-inch thick crust)"},
+    {user_id: 3, pizza_id: 7, pickup_name: "Betty Crocker", special_instructions: "Light buffalo sauce on top", size: "Mini (6-inch)"},
+    {user_id: 3, pizza_id: 8, pickup_name: "Betty Crocker", special_instructions: "Extra chicken ontop.", size: "Mod (11-inch)"}
+])
